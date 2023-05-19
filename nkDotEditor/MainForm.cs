98b56eda4dcb5fba55ae1537e117669092bc1c74
Victor <publicdomainvic@gmail.com>
2023-05-19 15:57:26 -0400

    Add OnExitToolStripMenuItemClick()

--- nkDotEditor/MainForm.cs
+++ nkDotEditor/MainForm.cs
@@ -342,7 +342,8 @@ namespace nkDotEditor
         /// <param name="e">Event arguments.</param>
         private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Close program
+            this.Close();
         }
     }
 }
